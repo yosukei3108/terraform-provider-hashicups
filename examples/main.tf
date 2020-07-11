3e879dee8b9cd4fc6baa2981802fe31680e09500
Judith Malnick <judith.patudith@gmail.com>
2020-07-10 17:44:06 -0700

    all the changes

--- examples/main.tf
+++ examples/main.tf
@@ -1,4 +1,8 @@
-provider "hashicups" {}
+provider "hashicups" {
+  username = "education"
+  password = "test1234"
+}
+
 
 module "psl" {
   source = "./coffee"
@@ -8,4 +12,12 @@ module "psl" {
 
 output "psl" {
   value = module.psl.coffee
+}
+
+data "hashicups_order" "order" {
+  id = 2
+}
+
+output "order" {
+  value = data.hashicups_order.order
 }
---
+++ go.mod
@@ -0,0 +1,8 @@
+module terraform-provider-hashicups
+
+go 1.13
+
+require (
+	github.com/hashicorp/terraform-plugin-sdk/v2 v2.0.0-rc.2
+	github.com/hashicorp/terraform-provider-hashicups v0.0.0-20200626075546-ebe6ad44df33
+)
