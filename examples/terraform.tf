ee523cf4355e56c54b9113bc5f38e4e4594d1859
Robin Norwood <rnorwood@hashicorp.com>
2021-05-25 19:18:09 -0500

    Update for 0.15

--- examples/terraform.tf
+++ examples/terraform.tf
@@ -1,8 +1,8 @@
 terraform {
-  required_version = ">= 0.13.0"
+  required_version = "~> 0.15.0"
   required_providers {
     hashicups = {
-      versions = ["0.3"]
+      version = "0.3"
       source = "hashicorp.com/edu/hashicups"
     }
   }
